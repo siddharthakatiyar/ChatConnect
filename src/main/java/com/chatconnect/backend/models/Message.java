4dab3806e5c8cd9eec0e000a5c73469dd2aaf79a
Siddhartha Katiyar <siddharthakatiyar25@gmail.com>
2023-07-17 20:32:11 +0530

    removed an unnecessary import

--- src/main/java/com/chatconnect/backend/models/Message.java
+++ src/main/java/com/chatconnect/backend/models/Message.java
@@ -1,7 +1,5 @@
 package com.chatconnect.backend.models;
 
-import java.time.LocalDateTime;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
