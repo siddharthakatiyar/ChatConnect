6dc7b7c875b3df6f55b32b1db00d362e329df336
Siddhartha Katiyar <siddharthakatiyar25@gmail.com>
2023-07-19 03:53:41 +0530

    update

--- src/main/java/com/chatconnect/backend/security/WebSecurityConfig.java
+++ src/main/java/com/chatconnect/backend/security/WebSecurityConfig.java
@@ -13,54 +13,67 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import com.chatconnect.backend.controllers.CCWebSocketHandler;
 import com.chatconnect.backend.security.jwt.AuthEntryPointJwt;
 import com.chatconnect.backend.security.jwt.AuthTokenFilter;
+import com.chatconnect.backend.security.jwt.WebSocketAuthInterceptor;
 import com.chatconnect.backend.security.services.UserDetailsServiceGet;
 
 @Configuration
 @EnableMethodSecurity
-public class WebSecurityConfig {
+@EnableWebSocket
+public class WebSecurityConfig implements WebSocketConfigurer {
 	@Autowired
 	UserDetailsServiceGet userDetailsService;
-	
+
 	@Autowired
 	private AuthEntryPointJwt unauthorizedHandler;
-	
+
 	@Bean
 	public AuthTokenFilter authenticationJwtTokenFilter() {
 		return new AuthTokenFilter();
 	}
-	
+
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
 		return authConfig.getAuthenticationManager();
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 		authProvider.setUserDetailsService(userDetailsService);
 		authProvider.setPasswordEncoder(passwordEncoder());
-		
+
 		return authProvider;
 	}
-	
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(new CCWebSocketHandler(), "/chat")
+				.setAllowedOrigins("*")
+				.addInterceptors(new WebSocketAuthInterceptor());
+	}
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
-        .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(auth -> 
-          auth.requestMatchers("/api/auth/**").permitAll()
-              .requestMatchers("/api/test/**").permitAll()
-              .anyRequest().authenticated()
-        );
+				.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(auth -> 
+				auth.requestMatchers("/api/auth/**").permitAll()
+					.requestMatchers("/api/test/**").permitAll()
+					.anyRequest().authenticated()
+				);
 		http.authenticationProvider(authenticationProvider());
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 		return http.build();
