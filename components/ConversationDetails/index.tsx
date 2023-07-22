515d8a8bc1cd25812a3f18a08bbf3773703cc968
Siddhartha Katiyar <siddharthakatiyar25@gmail.com>
2023-07-22 08:06:05 +0530

    socketio-client update

--- components/ConversationDetails/index.tsx
+++ components/ConversationDetails/index.tsx
@@ -3,50 +3,65 @@ import { ConversationContext } from "../../context/ConversationContext";
 import Avatar from "../Avatar";
 import MessageBalloon from "../MessageBalloon";
 import Send from "../MessageHandler/send";
+import { initiateSocket, getSocket } from "../../utils/websocket";
 
 export default function ConversationDetails() {
   const { conversation, message, setMessage } = useContext(ConversationContext);
   const { username, messageHistory } = conversation;
-  const [ messageSend, setMessageSend ] = useState("");
+  const [messageSend, setMessageSend] = useState("");
 
-  useEffect( () => {
+  useEffect(() => {
+
+    initiateSocket({ username });
     setMessage(messageHistory);
+    const socket = getSocket();
+
+    socket.on('receive_message', (data: any) => {
+      setMessage([...messageHistory, { me: false, message: data.message, created_at: data.created_at }]);
+    });
+
   }, [conversation]);
+
 
   function changeHandler(evt: KeyboardEvent<HTMLInputElement>) {
     const { key } = evt;
 
-    if ( key ==="Enter") {
+    if (key === "Enter") {
+      const socket = getSocket();
+      setMessage([...message, { me: true, message: messageSend, created_at: new Date().toISOString() }]);
+      socket.emit('send_message', {
+        receiver_username: username,
+        message: messageSend,
+        created_at: new Date().toISOString(),
+      });
       setMessageSend("");
-      setMessage( [...message, { me: true, message: messageSend, created_at: new Date().toISOString() }] );
-      Send( { username, message: messageSend } );
     }
   }
 
   return (
     <div className="flex flex-col w-full">
       <div className="flex justify-between w-full px-4">
-        <div className="flex justify-between bg-[#d9d9d9] w-full h-14">
+        <div className="flex justify-between bg-[#fbfaf5] w-full h-14 border-b border-[rgba(120,40,200,1)]">
           <div className="flex items-center gap-4 h-full">
             <Avatar width="w-10" height="h-10" />
             <h1 className="text-black font-normal">{username}</h1>
           </div>
         </div>
       </div>
-      <div className="flex flex-col w-full h-full px-24 py-6 overflow-y-auto" style={{background: "#e0e0e0"}}>
+      <div className="flex flex-col w-full h-full px-24 py-6 overflow-y-auto" style={{ backgroundImage: "url('/chatbackground.png')", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundColor: "#fbfaf5" }}>
         {
-          message.map( ( messageConversation, index ) => {
+          message.map((messageConversation, index) => {
             const { me, message, created_at } = messageConversation;
 
             return (
               <MessageBalloon key={index} me={me} message={message} created_at={created_at} />
             )
-          } )
+          })
         }
       </div>
-      <footer className="flex items-center bg-[#d9d9d9] w-full h-16 py-3 text-[#8696a0]">
+      <footer className="flex items-center bg-[#fbfaf5] w-full h-16 py-3 text-[#8696a0]">
         <div className="flex w-[97.5%] h-12 ml-3">
-          <input type={"text"} className="bg-[#e0e0e0] rounded-lg w-full px-3 py-3 text-white" placeholder="Type a message" onKeyDown={(evt) => changeHandler(evt) } onChange={ (evt) => setMessageSend(evt.target.value) } value={messageSend} />
+          <input type={"text"} className="bg-[#e0e0e0] rounded-lg w-full px-3 py-3 text-white" placeholder="Type a message" onKeyDown={(evt) => changeHandler(evt)} onChange={(evt) => setMessageSend(evt.target.value)} value={messageSend} />
         </div>
       </footer>
     </div>
---
+++ utils/websocket.tsx
@@ -0,0 +1,38 @@
+// utils/websocket.ts
+import io from 'socket.io-client';
+import router from "next/router";
+
+// Add the following line to import the type declaration
+import { Socket } from 'socket.io-client';
+
+let socket: Socket;
+
+interface websocketProps {
+    username: string;
+}
+
+export const initiateSocket = (websocketProps: websocketProps) => {
+    const token = localStorage.getItem('token');
+    const validity = localStorage.getItem('validity');
+    const date = localStorage.getItem('date');
+
+
+    const isTokenExpired = new Date().getTime() - Number(date) > Number(validity);
+
+    if (isTokenExpired || !token) {
+        localStorage.removeItem('jwtToken');
+        localStorage.removeItem('validity');
+        localStorage.removeItem('date');
+        router.push('/auth');
+        // throw new Error('Token expired. Please log in again.');
+    }
+    socket = io('ws://localhost:8082?code=' + websocketProps.username + '&token=' + token, {
+        transports: ['websocket'],
+    });
+    // console.log("ws://localhost:8082?code=" + websocketProps.username + "&token=" + token);
+
+};
+
+export const getSocket = () => {
+    return socket;
+};
